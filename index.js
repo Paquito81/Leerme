17d0fda0843d951fbc9571c8437650bb4da48925
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-03-23 16:50:41 -0700

    added the first questions

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('src/gaint.js');
+const pageHTML= generatePage (name,github);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+    console.log ('portfolio complete! checkout index.html to see output');
+    console.log(inquirer);
+});
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type:'input',
+            name:'name',
+            message:'What is your name?(Require)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'input',
+            name:'github',
+            message: 'Enter your Github Username (Required)',
+            validate: gitgubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github username!');
+                    return false;
+                }
+            }
+        }
+    ]);
+};
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
