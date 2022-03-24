4f22d1f1743930b5275fdc55bc197d48dade25e4
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-03-23 19:08:46 -0700

    deleted extra comments

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-//let answers = questions();
-//console.log(answers)
-//c//onst generatePage = require('src/gaint.js');
-//const pageHTML= generatePage (name,github);
-// //fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-//     console.log ('portfolio complete! checkout index.html to see output');
-//     console.log(inquirer);
-// });
 
 // TODO: Create an array of questions for user input
 const questions = [
