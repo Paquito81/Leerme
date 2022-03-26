7b58814a2f14c692cea05e562411f2d756688bbf
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-03-25 19:19:47 -0700

    adding more questions and updating gaint.js

--- index.js
+++ index.js
@@ -1,49 +1,65 @@
 // TODO: Include packages needed for this application
+//We import the 'fs' module to enable interactions with the file system
 const fs = require('fs');
 const inquirer = require('inquirer');
+//We import the 'star' folder to interact with the gaint.js and LICENSE file 
+const star = require('star');
+
+const generateTemplate = require('star/gaint.js');
 
 // TODO: Create an array of questions for user input
+// We create questions to generate answers that are going to be added to the README file
 const questions = [
     {
         type:'input',
-        name:'name',
+        name:'Name',
         message:'What is your name?', 
     },
     {
         type: 'input',
-        name:'github',
-        message: 'Enter your Github Username',
+        name:'Github',
+        message: 'Enter your Github Username.',
     },
     {
         type: 'input',
-        name:'project',
+        name:'Project',
         message: 'What is the project name?',
     },
     {
         type: 'input',
-        name:'description',
-        message: 'Enter a brief description of your project',
+        name:'Description',
+        message: 'Enter a brief description of your project.',
     },
     {
         type: 'input',
-        name:'instructions',
-        message: 'What commands can be used to install the app?',
+        name:'Instructions',
+        message: 'What commands are used to install the app?',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'What do you need to run this app?',
     },
     {
         type: 'list',
-        name:'license',
+        name:'License',
         message: 'What license would you like to use for the app?',
         choices: ['MIT', 'Apache', 'GPL', 'BSD']
     },
     {
         type: 'input',
-        name: 'email',
+        name: 'Test',
+        message: 'What command do you need to run this app?',
+    },
+    {
+        type: 'input',
+        name: 'Email',
         message: 'What is your email?',
     }
 ] 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err) {
+function writeToFile(fileName, answers) {
+    fs.writeFile(fileName, answers, function(err) {
         console.log(filename)
         console.log(answers)
         if(err) {
@@ -61,7 +77,9 @@ function writeToFile(fileName, data) {
     //});
 }
 
+//Use new function cause the old function was not generating a README file
 function init() {
+    console.log("Initializing App ...")
     inquirer.prompt(questions)
         .then(function(data) {
             writeToFile("README.md", generateTemplate(answers));
